8c16d7de6dd3fd98855b7c7bb867c680408b319c
Prabeen04 <prabeen.strange@gmail.com>
2019-01-16 21:20:54 +0530

    table read script

--- table-data-reader/app.js
+++ table-data-reader/app.js
@@ -1,35 +1,21 @@
-let table = document.getElementsByTagName('table')[0]
-let rowCount = 0;
-let wrongDataCount = 0;
-
-// console.log(table)
-// for (let row of table.rows) {
-//     if (row.getAttribute('style') === 'background-color: tomato') {
-//         rowCount++
-//     }
-//     // for (let cell of row.cells) {
-//     //     let value = cell.innerText;
-//     //     console.log(value)
-//     // }
-
-//     for (j = 0; j < 3; j++)
-//     {
-//         y = table[i].cells;
-//         //do something with cells in a row
-//         y[j].innerHTML = "";
-//     }
-// }
-for (var i = 0, row; row = table.rows[i]; i++) {
-    if (row.getAttribute('style') === 'background-color: tomato') {
-        rowCount++
+function solution(D, T) {
+    let table = document.getElementsByTagName('table')[0]
+    let rowCount = 0;
+    let wrongDataCount = 0;
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        if (row.getAttribute('style') === 'background-color: tomato') {
+            rowCount++
+        }
+        let list = row.getElementsByTagName('td')
+        let issuedDate = new Date(list[1].innerHTML);
+        let returnedDate = new Date(list[2].innerHTML);
+        if (issuedDate - returnedDate < 0) {
+            console.log('dates are same', returnedDate)
+            wrongDataCount++
+        }
     }
-    console.log(row)
-    //iterate through rows
-    //rows would be accessed using the "row" variable assigned in the for loop
-    for (var j = 0, col; col = row.cells[i]; j++) {
-        // console.log(col)
-        //iterate through columns
-        //columns would be accessed using the "col" variable assigned in the for loop
-    }
+    return rowCount - wrongDataCount
 }
-console.log(rowCount)
+
+
+console.log(solution(1, 2))
