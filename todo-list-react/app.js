8c8674b16da1eb135c01a5f48e984cb5ee36ac9b
Prabeen04 <prabeen.strange@gmail.com>
2019-01-16 21:33:25 +0530

    todo-list react snippet

---
+++ todo-list-react/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM, { render } from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>My React Component</h3>
+      </div>
+    )
+  }
+}
+
