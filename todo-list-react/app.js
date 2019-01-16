bd16f6f75bec61b53578ee0ec974eb0767a86a31
Prabeen04 <prabeen.strange@gmail.com>
2019-01-16 21:49:07 +0530

    react react-dom classnames added

--- todo-list-react/app.js
+++ todo-list-react/app.js
@@ -1,13 +1,16 @@
+console.log('inside App.js')
 import React, { Component } from 'react'
-import ReactDOM, { render } from "react-dom";
-
+import ReactDOM from "react-dom";
+console.log('inside App.js')
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h3>My React Component</h3>
-      </div>
-    )
-  }
+    render() {
+        console.log('inside render')
+        return (
+            <div>
+                <h3>My React Component</h3>
+            </div>
+        )
+    }
 }
 
+ReactDOM.render(<App />, document.getElementById('root'))
