39eb0117b126fcd984c829741bcfbeea6b6742e6
Prabeen04 <prabeen.strange@gmail.com>
2019-01-18 00:51:33 +0530

    checkbox stuff

--- profile-viewer/src/Components/UserList.js
+++ profile-viewer/src/Components/UserList.js
@@ -7,17 +7,19 @@ export class UserList extends Component {
     render() {
         const { peoples, setSelectedPeople, checkAllPeople, checkPeople, isAllSelected, deleteSelectedPeople } = this.props;
         console.log(peoples)
+        let ifAllUnchecked =  peoples.some((people) => people.isSelected === true)
+        console.log(ifAllUnchecked)
         return (
             <>
                 <div className='flex-container space-between'>
-                    <Checkbox checked={isAllSelected} onChange={checkAllPeople} />&nbsp;
+                    <Checkbox checked={isAllSelected } onChange={checkAllPeople} indeterminate={ifAllUnchecked && !isAllSelected} />&nbsp;
                 <Icon name='trash alternate' circular onClick={() => deleteSelectedPeople()} />
 
                 </div>
                 <List selection verticalAlign='middle'>
                     {peoples.map((people, i) => (
                         <List.Item key={i} onClick={() => setSelectedPeople(people)}>
-                            <Checkbox checked={people.isSelected} onChange={() => checkPeople(people.id)}/>&nbsp;
+                            <Checkbox checked={people.isSelected} onChange={() => checkPeople(people.id)} />&nbsp;
                             <Image avatar src={people.img} />
                             <List.Content>
                                 <List.Header>{people.name}</List.Header>
