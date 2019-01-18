0f602645cc4390de072f2ad013cd2f2623c6334a
Prabeen04 <prabeen.strange@gmail.com>
2019-01-18 21:29:24 +0530

    reduce recursion stuff

--- reduce/app.js
+++ reduce/app.js
@@ -4,33 +4,42 @@ let arr = [1, [2], [3, [[4]]]] //expected output:  [1, 2, 3, 4]
 //with for loop
 function flatten() {
     var flat = [];
-    for (var i=0; i<arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-        for (var ii=0; ii<arr[i].length; ii++) {
-        if (Array.isArray(arr[i][ii])) {
-            for (var iii=0; iii<arr[i][ii].length; iii++) {
-            for (var iiii=0; iiii<arr[i][ii][iii].length; iiii++) {
-                if (Array.isArray(arr[i][ii][iii])) {
-                flat.push(arr[i][ii][iii][iiii]);
+    for (var i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            for (var ii = 0; ii < arr[i].length; ii++) {
+                if (Array.isArray(arr[i][ii])) {
+                    for (var iii = 0; iii < arr[i][ii].length; iii++) {
+                        for (var iiii = 0; iiii < arr[i][ii][iii].length; iiii++) {
+                            if (Array.isArray(arr[i][ii][iii])) {
+                                flat.push(arr[i][ii][iii][iiii]);
+                            } else {
+                                flat.push(arr[i][ii][iii]);
+                            }
+                        }
+                    }
                 } else {
-                flat.push(arr[i][ii][iii]);
+                    flat.push(arr[i][ii]);
                 }
             }
-            }
         } else {
-            flat.push(arr[i][ii]);
+            flat.push(arr[i]);
         }
-        }
-    } else {
-    flat.push(arr[i]);
-    }
     }
     return flat
 }
 
 //with reduce
-const flatWithReduce= arr.reduce((acc, item) => {
-    return acc.concat(flatWithReduce(item))
-}, [])
-console.log(flatWithReduce)
+const flatWithReduce = function (array) {
+   return array.reduce((acc, item) => {
+       console.log('first loop', acc)
+        if (Array.isArray(item)) {
+            console.log('is array', item)
+            flatWithReduce(item)
+        } else {
+            acc.push(item)
+        }
+        return acc
+    }, [])
+}
+console.log(flatWithReduce(arr))
 console.log(flatten(arr))
