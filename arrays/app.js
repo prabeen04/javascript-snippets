5fbc562d087a7db956d807ffcb9d191ea4f00188
Prabeen04 <prabeen.strange@gmail.com>
2019-02-18 21:14:07 +0530

    no of duplicate item and its indexes calculated

--- arrays/app.js
+++ arrays/app.js
@@ -1,1 +1,22 @@
 const arr = [1, 2, 3, 4, 5, 5, 6, 11, 11, 11, 34, 65, 77]
+
+function findDuplicateAndItsIndexes(arr, num) {
+    if (!Array.isArray(arr) || !num) {
+        console.log('incorrect input')
+        return
+    }
+    const obj = {
+        count: 0,
+        indexes: []
+    }
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] === num) {
+            obj['count']++
+            obj['indexes'].push(i)
+        }
+    }
+
+    return obj;
+}
+
+console.log(findDuplicateAndItsIndexes(arr, 11))
