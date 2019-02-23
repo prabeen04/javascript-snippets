14d27c3953f5c17809f1fd9e3c20fd557c91a70a
Prabeen04 <prabeen.strange@gmail.com>
2019-02-23 20:27:21 +0530

    add method in Linked list

--- data-structure/LinkedList/app.js
+++ data-structure/LinkedList/app.js
@@ -1,9 +1,22 @@
 class LinkedList {
-    constructor(){
+    constructor() {
         this.data = null;
         this.length = 0;
+    }
+    add(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.data = newNode;
+        this.length++;
+    }
+
+    remove() {
+
     }
 }
 
 const myLinkedList = new LinkedList();
+myLinkedList.add('test')
 console.log(myLinkedList)
