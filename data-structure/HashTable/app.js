900dfaa511d057437cb8103c1bf21e9bf20c3ab7
Prabeen04 <prabeen.strange@gmail.com>
2019-02-23 12:11:24 +0530

    hash table implementation

---
+++ data-structure/HashTable/app.js
@@ -0,0 +1,20 @@
+class HashTable {
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+
+    _hash(key){
+        //generate a hash
+    }
+
+    set(key, value){
+        //set the key value pair
+    }
+
+    get(key){
+        //gey the value of the key
+    }
+}
+
+console.log(new HashTable)
