cd69e7e2b29b86749fc360d050e64a140e319bd1
Prabeen04 <prabeen.strange@gmail.com>
2019-07-17 18:34:57 +0530

    data types

---
+++ DataTypes/app.js
@@ -0,0 +1,44 @@
+//HOISTING
+
+//data types
+//string
+//number
+//undefined
+//null
+//object -> array, function
+//Symbol
+/**
+ * var a; //undefined
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// css engine
+// html DOM
+// js engine //chrome: v8, microsoft: chakra, mozilla: spider monkey
+//web apis
+
+// print('235235')
+// console.log(a)
+// a = 45;
+// console.log(a)
+// var a = 56;
+
+// function print(arg){
+//     console.log('arg', arg)
+// }
+
+
+
+// const test = [1, 2, 3, 4];
+// // let test2 = 4;
+
+// function scope() {
+//     return test2
+// }
+// let test2 = 67;
+// console.log(scope())
+
